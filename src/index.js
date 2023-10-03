92c8ca56b9f0a5c517a29eefefd82e23c41bae5c
unknown <parimalkokode@gmail.com>
2023-10-03 12:51:56 +0530

    Fixed frontend bug

--- src/index.js
+++ src/index.js
@@ -8,12 +8,7 @@ import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducer";
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://study-notion-an-ed-tech-platform-seven.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 
 const store = configureStore({
   reducer: rootReducer,
